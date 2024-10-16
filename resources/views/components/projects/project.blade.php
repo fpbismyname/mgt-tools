bc13976bf72181145d91c3baa64d350bfc464798
fpbismyname <fajarpebefajar12@gmail.com>
2024-10-16 18:27:55 +0700

    Updated 1.0.1

--- resources/views/components/projects/project.blade.php
+++ resources/views/components/projects/project.blade.php
@@ -8,7 +8,33 @@
     @case('Solution Domain')
         <x-projects.solution-domain/>
     @break
-
+    @case('Potential Problem')
+        <x-projects.potential-problem/>
+    @break
+    @case('Requirement Revision')
+        <x-projects.requirement-revision/>
+    @break
+    @case('Need vs Feat')
+        <x-projects.need-vs-feat/>
+    @break
+    @case('Feasibility')
+        <x-projects.feasibility/>
+    @break
+    @case('Finalization')
+        <x-projects.finalization/>
+    @break
+    @case('Use Case')
+        <x-projects.use-case/>
+    @break
+    @case('Use Case')       
+        <x-projects.use-case/>
+    @break
+    @case('Use Case vs Actor')       
+        <x-projects.use-case-vs-actor/>
+    @break
+    @case('Use Case vs Feat')       
+        <x-projects.use-case-vs-feat/>
+    @break
     @default
         <x-projects.project-description :project="$project" />
 @endswitch
