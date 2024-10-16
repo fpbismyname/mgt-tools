ceaeb133d1de9088fddf9cfcd1a02179b43c873f
fpbismyname <fajarpebefajar12@gmail.com>
2024-10-16 17:05:43 +0700

    Updated 1.0.1

--- resources/views/components/projects/show-project.blade.php
+++ resources/views/components/projects/show-project.blade.php
@@ -1,5 +1,6 @@
 <x-layout title="{{ $project->project_name }}">
     <x-navbar />
     <x-projects.project-header :projectApp="$project" :projectMenu="$projectMenu" :selectedMenu="$selectedMenu"/>
-    <x-projects.project :project='$project' :menu='$selectedMenu'/>
+    {{-- <x-projects.project :project='$project' :menu='$selectedMenu'/> --}}
+    <h1>Kumalalaa Sasvesta</h1>
 </x-layout>
